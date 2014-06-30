e18a37532295b1d9fc3069f686bce562aea8ccb7
Lane <zhanglintc623@gmail.com>
2014-06-30 22:41:37 +0800

    readable

--- chatroomServer.py
+++ chatroomServer.py
@@ -32,32 +32,34 @@ message_queues = {}
 #A optional parameter for select is TIMEOUT
 timeout = 20000
 
+server_map = {}
+
 def func():
+    data = None
     while True:
-        print('System: server now is listening on port ' + str([port]))
+        #print('System: server now is listening on port ' + str([port]))
         readable , writable , exceptional = select.select(inputs, outputs, inputs, timeout)
         if not (readable or writable or exceptional) :
-                print ("Time out ! ")
-                break
+            print ("Time out ! ")
+            break
         for s in readable :
             if s is server:
                 # A "readable" socket is ready to accept a connection
                 connection, client_address = s.accept()
-                print ("    connection from ", client_address)
+                print (str(client_address) + ' logged in')
+                server_map[connection] = client_address
                 connection.setblocking(0)
                 inputs.append(connection)
-                message_queues[connection] = Queue.Queue()
             else:
-                data = s.recv(1024).decode()
-                if data :
-                    print (" received " , [data] , "from ",s.getpeername())
-                else:
-                    #Interpret empty result as closed connection
-                    print ("  closing", client_address)
+                try:
+                    data = s.recv(1024).decode()
+                except:
+                    print ("  closing", server_map[s])
                     inputs.remove(s)
                     s.close()
-                    #remove message queue 
-                    del message_queues[s]
+                else:
+                    if data :
+                        print (" received " , [data] , "from ",s.getpeername())
 
 if __name__ == '__main__':
     func()
